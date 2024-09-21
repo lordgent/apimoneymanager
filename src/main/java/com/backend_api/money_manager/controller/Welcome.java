7cf64e9b5ef7f510324c7a06a0b13d922e0353b3
lordgent1 <lordgent@digitalcenter.id>
2024-09-21 14:05:16 +0700

    init

--- src/main/java/com/backend_api/money_manager/controller/Welcome.java
+++ src/main/java/com/backend_api/money_manager/controller/Welcome.java
@@ -2,7 +2,6 @@ package com.backend_api.money_manager.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
