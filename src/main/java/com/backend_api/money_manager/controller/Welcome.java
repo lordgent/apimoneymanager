802af55de9b7822170eaefd4869aa8ea7f6450c5
lordgent1 <lordgent@digitalcenter.id>
2024-09-21 13:38:49 +0700

    init project

---
+++ src/main/java/com/backend_api/money_manager/controller/Welcome.java
@@ -0,0 +1,19 @@
+package com.backend_api.money_manager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class Welcome {
+
+    @GetMapping("")
+    public ModelAndView welcome(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("welcome/index");
+        return modelAndView;
+    }
+
+}
+
---
+++ src/test/java/com/backend_api/money_manager/MoneyManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend_api.money_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
